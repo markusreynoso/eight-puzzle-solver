45a3732a47dd3d1dd64c24c976a02030b744b0cc
Markus <markus.reynoso.c@gmail.com>
2024-04-24 10:44:10 +0800

    Added UI

--- Main.java
+++ Main.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Grid grid = new Grid("58761324 ");
+        Grid demoGrid = new Grid("123 45678");
+        Scanner sc = new Scanner(System.in);
         Solver solver = new Solver();
+        String layout = "";
+
+        System.out.println("Input the layout with a space to denote the blank tile. For example, 123 45678 is read as:");
+        System.out.println(demoGrid.getLayoutPrint());;
+
+        System.out.print("Layout: ");
+        layout = sc.nextLine();
+        Grid grid = new Grid(layout);
+
         System.out.println(grid.getLayoutPrint());
+        System.out.println("BFS:");
         System.out.println(solver.solvePuzzleBfs(grid));
+        System.out.println("A-Star:");
+        System.out.println(solver.solvePuzzleAStar(grid));
     }
 }
